6cb4b339d51afd4e43cafd81001d1a373bfb55b9
Nafisa Schofield <schofield.n.nyc@gmail.com>
2018-06-23 20:15:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // write your code below!
 
 var name = "Nafisa";
-var height = "42";
-var weight
+var height = "6'3";
+var weight = "125 lb";
 
-console.log("Name:");
+console.log("Lady's Name:");
 console.log(name);
 
-console.log("Height:");
+console.log("Nafisa's Height:");
 console.log(height);
 
+console.log ("Weight of Nafisa");
+console.log ("weight");
 
 
