94529856364dc38af13049575739af63d9e33750
Nafisa Schofield <schofield.n.nyc@gmail.com>
2018-06-23 20:06:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 var name = "Joe";
 var height = "74";
 
-console.log("Name:");
+//console.log("Name:");
 console.log(name);
 
-console.log("Height:");
-console.log(height);
+//console.log("Height:");
+//console.log(height);
 console.log("name")
 
